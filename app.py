dbf86f926e812585f13eca8336e813072526c873
zaidi0069 <93919683+zaidi0069@users.noreply.github.com>
2024-03-30 21:06:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,21 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
-
-#create flask app
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
-
-#load the pickle model
 
 model = pickle.load(open("model.pkl", "rb"))
 
 @app.route("/")
-def Home():
+def home():
     return render_template("index.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
     features = [float(x) if i != 2 else x for i, x in enumerate(request.form.values())]
-    features[2] = 1 if features[2].lower() == 'yes' else 0  # Convert 'Yes'/'No' to binary
-    features = np.array(features).reshape(1, -1)  # Reshape to 2D array
+    features[2] = 1 if features[2].lower() == 'yes' else 0
+    features = np.array(features).reshape(1, -1)
     prediction = model.predict(features)
     return render_template("index.html", prediction_text="Student Performance Index is: {}".format(prediction[0]))
 
-
-
-if __name__ =="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
